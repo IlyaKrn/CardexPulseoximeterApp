fa83dfea5463e9eaafe661136c59edb8996dcb98
IlyaKrn <ilyakornoukhov@gmail.com>
2025-06-30 04:02:13 +0300

    list of devices (without updating)

--- app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesFragment.kt
+++ app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesFragment.kt
@@ -1,43 +1,49 @@
 package com.ilyakrn.cardexpulseoximeterapp.ui.bottomnav.devices
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ilyakrn.cardexpulseoximeterapp.databinding.FragmentNavDevicesBinding
-import com.ilyakrn.cardexpulseoximeterapp.models.DeviceModel
 import com.ilyakrn.cardexpulseoximeterapp.ui.adapters.DeviceAdapter
 
 class DevicesFragment : Fragment() {
 
     private lateinit var binding: FragmentNavDevicesBinding
+    private lateinit var viewModel: DevicesViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val viewModel = ViewModelProvider(this)[DevicesViewModel::class.java]
+        viewModel = ViewModelProvider(this)[DevicesViewModel::class.java]
         binding = FragmentNavDevicesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
         val adapter = DeviceAdapter()
-        adapter.devices = arrayListOf(DeviceModel("mac:mac", "name1", true), DeviceModel("mac:mac", "name1", true), DeviceModel("mac:mac", "name1", false))
-        adapter.onClickListener = { device ->
-            Toast.makeText(context, device.address, Toast.LENGTH_SHORT).show()
-        }
         binding.rvDevices.layoutManager = LinearLayoutManager(activity)
         binding.rvDevices.adapter = adapter
-
-
-
-        viewModel.text.observe(viewLifecycleOwner) {
-
+        binding.btReload.setOnClickListener {
+            viewModel.updateDevices()
         }
 
+        viewModel.devices.observe(viewLifecycleOwner) { devices ->
+            adapter.devices = devices
+        }
+        viewModel.enableBluetooth(this)
+        viewModel.getDevices(requireContext())
 
-        return root
+        return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == RESULT_OK) {
+            when (requestCode) {
+                1 -> viewModel.getDevices(requireContext())
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesViewModel.kt
+++ app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesViewModel.kt
@@ -1,11 +1,40 @@
 package com.ilyakrn.cardexpulseoximeterapp.ui.bottomnav.devices
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ilyakrn.cardexpulseoximeterapp.models.DeviceModel
 
 class DevicesViewModel : ViewModel() {
 
-    val text = MutableLiveData<ArrayList<String>>().apply {
+    val devices = MutableLiveData<ArrayList<DeviceModel>>().apply {
         value = ArrayList()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getDevices(context: Context){
+        getSystemService(context, BluetoothManager::class.java)?.adapter?.let {
+            if (it.isEnabled)
+                devices.value = it.bondedDevices.map { device -> DeviceModel(device.address.toString(), device.name, false) } as ArrayList<DeviceModel>
+        }
+    }
+
+    fun updateDevices(){
+
+    }
+
+    fun enableBluetooth(fragment: androidx.fragment.app.Fragment){
+        getSystemService(fragment.requireContext(), BluetoothManager::class.java)?.adapter?.let {
+            if (!it.isEnabled) {
+                fragment.startActivityForResult(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1)
+            }
+        }
     }
 }
