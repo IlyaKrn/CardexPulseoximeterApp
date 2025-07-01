7c5a1d7d7be75d743410b3939e0fc0dd94b8f429
IlyaKrn <ilyakornoukhov@gmail.com>
2025-07-01 04:38:55 +0300

    device bound

--- app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesFragment.kt
+++ app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesFragment.kt
@@ -1,18 +1,9 @@
 package com.ilyakrn.cardexpulseoximeterapp.ui.bottomnav.devices
 
-import android.Manifest
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.RequiresPermission
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,39 +20,24 @@ class DevicesFragment : Fragment() {
         viewModel = ViewModelProvider(this)[DevicesViewModel::class.java]
         binding = FragmentNavDevicesBinding.inflate(inflater, container, false)
         adapter = DeviceAdapter()
-        binding.rvDevices.layoutManager = LinearLayoutManager(context)
+        binding.rvDevices.layoutManager = LinearLayoutManager(activity)
         binding.rvDevices.adapter = adapter
         viewModel.devices.observe(viewLifecycleOwner) { devices ->
             adapter.devices = devices
         }
         adapter.onClickListener = { device ->
-            Toast.makeText(requireContext(), device.address, Toast.LENGTH_SHORT).show()
+            viewModel.connectToDevice(requireActivity(), device.address)
         }
         binding.btReload.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
-                return@setOnClickListener
-            }
-            viewModel.getDevices(requireContext())
+            viewModel.startDevicesScan(requireActivity())
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
-        } else{
-            viewModel.getDevices(requireContext())
-        }
+        viewModel.startDevicesScan(requireActivity())
 
         return binding.root
     }
 
-    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK) {
-            when (requestCode) {
-                1 -> {
-                    viewModel.getDevices(requireContext())
-                }
-            }
-        }
+    override fun onDestroy() {
+        viewModel.stopDevicesScan(requireActivity())
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesViewModel.kt
+++ app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/ui/bottomnav/devices/DevicesViewModel.kt
@@ -2,18 +2,39 @@ package com.ilyakrn.cardexpulseoximeterapp.ui.bottomnav.devices
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.os.BuildCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.lifecycle.viewModelScope
+import com.ilyakrn.cardexpulseoximeterapp.R
 import com.ilyakrn.cardexpulseoximeterapp.models.DeviceModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.util.UUID
+
 
 class DevicesViewModel : ViewModel() {
 
@@ -21,12 +42,79 @@ class DevicesViewModel : ViewModel() {
         value = ArrayList()
     }
 
-    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
-    fun getDevices(context: Context){
-        val newDevices = ArrayList<DeviceModel>()
-        getSystemService(context, BluetoothManager::class.java)!!.adapter!!.bondedDevices.forEach {
-            newDevices.add(DeviceModel(it.address, it.name, false))
+    lateinit var adapter: BluetoothAdapter
+
+    lateinit var receiver: BroadcastReceiver
+
+    private var isReceiverRegistered = false
+    private var isScan = false
+
+    fun startDevicesScan(activity: Activity){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(activity, arrayOf(Manifest.permission.BLUETOOTH_SCAN,), 1)
+            return
         }
-        devices.value = newDevices
+        if(ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            return
+        }
+
+        adapter = getSystemService(activity, BluetoothManager::class.java)!!.adapter!!
+        if(!isReceiverRegistered) {
+            receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context, intent: Intent) {
+                    if (intent.action == BluetoothDevice.ACTION_FOUND) {
+                        intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)?.apply {
+                            val newDevices = ArrayList(devices.value!!)
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                                requestPermissions(activity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
+                                return
+                            }
+                            if(name != null) {
+                                newDevices.add(DeviceModel(address, name, false))
+                                devices.value = newDevices
+                            }
+                        }
+                    }
+                }
+            }
+            activity.registerReceiver(receiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
+            isReceiverRegistered = true
+        }
+        devices.value = ArrayList()
+        if(!isScan) {
+            adapter.startDiscovery()
+            Toast.makeText(activity, "Поиск устройств начался", Toast.LENGTH_SHORT).show()
+            isScan = true
+            viewModelScope.launch {
+                delay(15000)
+                isScan = false
+            }
+        } else {
+            Toast.makeText(activity, "Поиск уже ведется", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun stopDevicesScan(activity: Activity){
+        if(isReceiverRegistered)
+            activity.unregisterReceiver(receiver)
+    }
+
+    fun connectToDevice(activity: Activity, address: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(activity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
+            return
+        }
+
+        adapter = getSystemService(activity, BluetoothManager::class.java)!!.adapter!!
+        val device = adapter.getRemoteDevice(address)
+        if(device.bondState == BluetoothDevice.BOND_NONE){
+            device.createBond()
+            return
+        }
+    }
+
+
 }
