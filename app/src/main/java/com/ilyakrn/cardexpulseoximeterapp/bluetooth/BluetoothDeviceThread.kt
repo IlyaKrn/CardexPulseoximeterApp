5a995d2129c640ac718ff890b9d51fd3f4edc76c
IlyaKrn <ilyakornoukhov@gmail.com>
2025-07-09 15:44:50 +0300

    device thread logs

--- app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/bluetooth/BluetoothDeviceThread.kt
+++ app/src/main/java/com/ilyakrn/cardexpulseoximeterapp/bluetooth/BluetoothDeviceThread.kt
@@ -1,20 +1,13 @@
 package com.ilyakrn.cardexpulseoximeterapp.bluetooth
 
 import android.Manifest
-import android.R.attr.text
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothSocket
 import android.util.Log
 import androidx.annotation.RequiresPermission
 import com.ilyakrn.cardexpulseoximeterapp.models.MeasureModel
-import java.io.DataInputStream
-import java.io.DataOutputStream
 import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-import java.util.Arrays
 import java.util.UUID
-
 
 class BluetoothDeviceThread(private val adapter: BluetoothAdapter, private val deviceAddress: String, private val onReadMeasure: (MeasureModel) -> Unit) : Thread() {
 
@@ -22,52 +15,82 @@ class BluetoothDeviceThread(private val adapter: BluetoothAdapter, private val d
 
     @RequiresPermission(allOf = [Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT])
     override fun run() {
-        adapter.cancelDiscovery()
-        val device = adapter.getRemoteDevice(deviceAddress)
+        try {
+            Log.i(this.javaClass.name, "socket thread started (trying mac '$deviceAddress')")
+            if (adapter.cancelDiscovery())
+                Log.i(this.javaClass.name, "discovery cancelled successful")
+            else
+                Log.w(this.javaClass.name, "discovery not canceled")
 
-        device.uuids?.let { uuids ->
-            for (i in 0..uuids.size){
-                try {
-                    socket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuids[i].toString()))
-                    socket.connect()
-                    break
-                } catch (e: Exception){}
-            }
-        }
-        if(socket.isConnected){
-            socket.inputStream.apply {
-                while (true){
+            val device = adapter.getRemoteDevice(deviceAddress)
+            Log.i(this.javaClass.name, "trying set connection with device '${device.name}', mac: '${device.address}', state: '${device.bondState}'")
+
+            device.uuids?.let { uuids ->
+                for (i in 0..uuids.size){
                     try {
-                        //read DATA1,
-                        var buffer = ""
-                        while (!buffer.endsWith("DATA1,")){
-                            buffer += read().toChar()
-                        }
-                        //read size
-                        buffer = ""
-                        while (!buffer.endsWith(",")){
-                            buffer += read().toChar()
-                        }
-                        val dataSize = buffer.substring(0, buffer.length - 1).toInt() + 5
-                        val dataBytesBuffer = ByteArray(dataSize)
-                        read(dataBytesBuffer, 0, dataSize)
-                        buffer = ""
-                        dataBytesBuffer.forEach { buffer += it.toChar() }
-
-                        val args = buffer.split(",")
-                        if(args.size != 6)
-                            continue
-                        onReadMeasure.invoke(MeasureModel(0, device.address, device.name, args[2], args[4].toInt(), args[5].toInt()))
-                    } catch(e : Exception){}
+                        Log.i(this.javaClass.name, "${device.address}: trying device uuid '${uuids[i]}'")
+                        socket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuids[i].toString()))
+                        socket.connect()
+                        Log.i(this.javaClass.name, "${device.address}: connection with uuid '${uuids[i]}' successful")
+                        break
+                    } catch (e: Exception){
+                        Log.w(this.javaClass.name, "${device.address}: failed to connect with uuid '${uuids[i]}'")
+                    }
                 }
-
             }
+            if(socket.isConnected){
+                socket.inputStream.apply {
+                    Log.i(this.javaClass.name, "${device.address}: start listening socket")
+                    while (true){
+                        try {
+                            //read DATA1,
+                            var buffer = ""
+                            while (!buffer.endsWith("DATA1,")){
+                                buffer += read().toChar()
+                            }
+                            Log.i(this.javaClass.name, "${device.address}: 'DATA1,' readed, trying to read data size")
+                            //read size
+                            buffer = ""
+                            while (!buffer.endsWith(",")){
+                                buffer += read().toChar()
+                            }
+                            val dataSize = buffer.substring(0, buffer.length - 1).toInt() + 5
+                            Log.i(this.javaClass.name, "${device.address}: data size read ($dataSize), trying to read args")
+                            val dataBytesBuffer = ByteArray(dataSize)
+                            read(dataBytesBuffer, 0, dataSize)
+                            buffer = ""
+                            dataBytesBuffer.forEach { buffer += it.toChar() }
+
+                            val args = buffer.split(",")
+                            if(args.size != 6) {
+                                Log.w(this.javaClass.name, "${device.address}: invalid args read, listening continued")
+                                continue
+                            }
+                            Log.i(this.javaClass.name, "${device.address}: start measure callback")
+                            onReadMeasure.invoke(MeasureModel(0, device.address, device.name, args[2], args[4].toInt(), args[5].toInt()))
+                        } catch(e : Exception){
+                            Log.w(this.javaClass.name, "${device.address}: error parsing socket input data, listening continued")
+                        }
+                    }
+
+                }
+            }
+            else{
+                Log.e(this.javaClass.name, "failed to connect to device '${device.name}', mac: '${device.address}'")
+            }
+        } catch (e: IllegalStateException){
+            Log.e(this.javaClass.name, "failed to connect to device: invalid mac '$deviceAddress'")
+        }catch (e: Exception){
+            e.printStackTrace()
         }
     }
 
     fun cancel() {
         try {
+            Log.w(this.javaClass.name, "$deviceAddress: bluetooth socket closed")
             socket.close()
-        } catch (e: IOException) {}
+        } catch (e: IOException) {
+            Log.w(this.javaClass.name, "$deviceAddress: failed to close socket: ${e.message}")
+        }
     }
 }
